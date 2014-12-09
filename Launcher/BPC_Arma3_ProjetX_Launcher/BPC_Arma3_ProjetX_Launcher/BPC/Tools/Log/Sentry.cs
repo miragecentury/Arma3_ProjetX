24acb5cd49e1743c355f2b07bd04dc59488818e5
VANROYE Victorien <victorien.vanroye@gmail.com>
2014-12-09 21:23:45 +0100

    Init Launcher

---
+++ Launcher/BPC_Arma3_ProjetX_Launcher/BPC_Arma3_ProjetX_Launcher/BPC/Tools/Log/Sentry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPC_Arma3_ProjetX_Launcher.BPC.Tools.Log
+{
+    class Sentry
+    {
+    }
+}
