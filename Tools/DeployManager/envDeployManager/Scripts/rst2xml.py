b95add48a5b6f887e1e33dc442462c99454c7f80
VANROYE Victorien <victorien.vanroye@gmail.com>
2014-12-10 23:20:32 +0100

    - Add Sphinx in virtenv

---
+++ Tools/DeployManager/envDeployManager/Scripts/rst2xml.py
@@ -0,0 +1,5 @@
+#!E:\Documents\GitHub\Arma3_ProjetX\Tools\DeployManager\envDeployManager\Scripts\python.exe
+# EASY-INSTALL-SCRIPT: 'docutils==0.12','rst2xml.py'
+__requires__ = 'docutils==0.12'
+import pkg_resources
+pkg_resources.run_script('docutils==0.12', 'rst2xml.py')
---
+++ Tools/DeployManager/envDeployManager/Scripts/sphinx-autogen-script.py
@@ -0,0 +1,10 @@
+#!E:\Documents\GitHub\Arma3_ProjetX\Tools\DeployManager\envDeployManager\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'sphinx==1.3b2','console_scripts','sphinx-autogen'
+__requires__ = 'sphinx==1.3b2'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('sphinx==1.3b2', 'console_scripts', 'sphinx-autogen')()
+    )
